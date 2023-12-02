23ee2c99480270c14646b6ae2b36c38f4e142d5f
deepak-getpu <deepak.getpu@gmail.com>
2023-12-02 02:08:54 -0500

    modified a code

--- src/week03/week03.java
+++ src/week03/week03.java
@@ -1,7 +1,7 @@
 package week001week001;
 
 public class Demoweek001week001 {
-
+// New program
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
